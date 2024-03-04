c5f55b2e1b0d2a5bb5337b0df14683452d598e76
dujichong <dujichong@didiglobal.com>
2024-03-04 23:34:25 +0800

    textInput

--- App.tsx
+++ App.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, StyleSheet} from 'react-native';
 // import Index01 from './src_01_StyleSheet/index01';
 // import Index02 from './src_02_flexbox/index';
 // import Index03 from './src_03_Dimensions/index';
-import Index04 from './src_04_alert_button';
+// import Index04 from './src_04_alert_button';
+import Index08 from './src_08_textInput';
+
+const styles = StyleSheet.create({
+  h2: {
+    fontSize: 40,
+    padding: 40,
+    textAlign: 'center',
+    lineHeight: 100,
+  },
+})
 
 export default function App() {
   return (
     <>
-      <Text>App</Text>
+      <Text style={styles.h2}>hello native</Text>
       {/* <Index01 />
       <Index02 /> */}
       {/* <Index03 /> */}
-      <Index04 />
+      {/* <Index04 /> */}
+      <Index08 />
     </>
   );
 }
---
+++ src_08_textInput/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Text, StyleSheet, View, Dimensions, TextInput } from 'react-native';
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+// 更多组件介绍：https://reactnative.dev/docs/textinput
+
+
+export default function App() {
+  // onChangeText 就是函数，可以直接传入，免得再定义
+  const [text, onChangeText] = React.useState('Useless Text');
+  const [number, onChangeNumber] = React.useState('');
+
+  const myChangeText = val => {
+    console.log('djch val', val)
+    onChangeText(val)
+  }
+
+  return (
+    <>
+      <View>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={myChangeText}
+          value={text}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="useless placeholder"
+          keyboardType="numeric"
+        />
+      </View>
+    </>
+  );
+}
